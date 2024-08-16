3857eec1aa7601062dd9967e6ee581c048aeb0ef
Anton Vasilev <anton.vasilev@jetbrains.com>
2024-08-16 23:21:14 +0300

    Chapter Real-time updates with Turbo Streams

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,24 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self } }
+  #  this ↑ is equivalent of after_create_commit -> { broadcast_prepend_to "quotes" }
+
+  # after_create_commit -> { broadcast_prepend_to "quotes" }
+  # after_update_commit -> { broadcast_replace_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+  # It is possible to improve the performance of this code by making the broadcasting part asynchronous
+  # using background jobs. To do this, we only have to update the content of our callbacks
+  # to use their asynchronous equivalents:
+
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+  # Those three callbacks are equivalent to the following single line:
+
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+
 end
