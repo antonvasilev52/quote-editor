2af81194aba01f5d190fd0b77b572eadfcb82c17
Anton Vasilev <anton.vasilev@jetbrains.com>
2024-08-30 11:27:09 +0300

    Hide Delete and Edit button if not mine

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -1,7 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-
     // This function is triggered every time a quote is added to the DOM.
     connect() {
         this.applyAuthorClasses()
@@ -13,13 +12,17 @@ export default class extends Controller {
         const authorId = this.element.getAttribute('data-author-id');
 
 
+        const messageButtons = this.element.querySelector('.quote__actions')
+
         // Compare the IDs and apply the class accordingly
         if (currentUserId === authorId) {
             this.element.classList.add("quote--mine")
         } else {
             this.element.classList.add("quote--his")
-        }
 
+            // Hide Delete and Edit button if not mine
+            messageButtons.classList.add("quote__hidden")
+        }
 
     }
 }
